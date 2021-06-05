a98c37926590677eff8e09d4f0608df6bdfd43fd
gudluckyadav <85403883+gudluckyadav@users.noreply.github.com>
2021-06-05 20:29:20 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Completed 7 Days Javascript and Reactjs Bootcamp with ShapeAI. It was an
+        amazing experience. Thank You
+      </p>
+    </div>
+  );
+}
+export default Note;
